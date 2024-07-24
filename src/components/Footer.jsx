044bf78477470d41d0a294508f7f77a7038b573a
Exynos13 <alexpeterthomas@gmail.com>
2024-07-24 17:27:44 +1000

    Footer full change commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import SocialIcons from "./SocialIcons";
 
 /**
@@ -7,51 +6,15 @@ import SocialIcons from "./SocialIcons";
  * @component
  */
 
-const Footer = () => {
-  const date = new Date();
-  const currentYear = date.getFullYear();
-
-  // State to hold GitHub information
-//  const [gitHubInfo, setGitHubInfo] = useState({
-//    stars: null,
-//    forks: null,
-//  });
-
-  useEffect(() => {
-    // Fetch GitHub repository information
-    fetch("https://api.github.com/repos/mdyeates/my-portfolio")
-      .then((res) => res.json())
-      .then((json) => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch((e) => console.error(e));
-  }, []);
-
-  // Variants for button animation
-//  const buttonVariants = {
-//    hover: {
-//      scale: 1.05,
-//    },
-//    tap: {
-//      scale: 1,
-//    },
-//  };
+const Footer = () => {  
 
   return (
     <footer>
       {/* Social icons */}
       <SocialIcons />
 
-      {/* GitHub repository link */}
-      <a className="footer-link" href="https://github.com/mdyeates/my-portfolio">
-        <p>
-          <span>▷</span> Template by Michael Yeates &copy; {currentYear}
-        </p>
-      </a>
+      
+      
     </footer>
   );
 };
