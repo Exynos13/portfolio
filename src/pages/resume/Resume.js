966d39c89a985d04f6c7082b22462df5306af6ad
Exynos13 <alexpeterthomas@gmail.com>
2024-07-31 19:55:36 +1000

    resume commit

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,9 +1,11 @@
 import PageHeader from "../../components/PageHeader";
 import Button from '../../components/Button';
 import Footer from "../../components/Footer";
+
 const Resume = ({ brand }) => {
   // const resumeLink = ''; // Replace with the actual link
-  const Downloadlink = process.env.PUBLIC_URL+ "/pdf/Resume.pdf"
+  const Downloadlink = process.env.PUBLIC_URL + "/pdf/Resume.pdf";
+  
   return (
     <section className="resume container">
       <PageHeader title="Resume" description="Uncover my expertise" />
@@ -11,20 +13,19 @@ const Resume = ({ brand }) => {
 
       {/* Display buttons in a row with gap */}
       <div className="button-row">
-        <a href={Downloadlink} className="view-cv-link" target="_blank" rel="noopener noreferrer" >
+        <a href={Downloadlink} className="view-cv-link" target="_blank" rel="noopener noreferrer">
           <Button name="View CV" color="#3498db" />
         </a>
         <div className="button-gap" />
         <a href={Downloadlink} className="Download-cv-link" download="Resume.pdf" target="_blank" rel="noopener noreferrer">
-           <Button name="Download CV" color="#2ecc71" />
+          <Button name="Download CV" color="#2ecc71" />
         </a>
         <br />
       </div>
       <div style={{ height: '100px' }}></div> {/* Adjust the height as needed for spacing */}
-
+      
       <Footer />
     </section>
-    
   );
 };
 
